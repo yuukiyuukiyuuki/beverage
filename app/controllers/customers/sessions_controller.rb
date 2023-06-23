92d0c3baf3e7f13f876f27308cdd0f4761f4c558
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2023-06-23 17:27:05 +0000

    Fix veiw

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -3,6 +3,6 @@ class Customers::SessionsController < Devise::SessionsController
   def guest_sign_in
     customer = Customer.guest
     sign_in customer
-    redirect_to root_path, notice: 'guestuserでログインしました。'
+    redirect_to customer_path(customer.id), notice: 'guestcustomerでログインしました。'
   end
 end
